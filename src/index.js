b89bc4cf2de5b0279b73d91ac610f7575ef6c9b1
mcrd25 <mayacrd25@gmail.com>
2020-01-07 02:28:33 -0400

    Removed unecessary files and added app title

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 
 import * as serviceWorker from './serviceWorker';
 
+function Title(props) {
+	return ( 
+		<h1>{props.value}</h1>
+	);
+}
 
+ReactDOM.render( <Title value='My React Calculator' />, document.getElementById('root'));
 
 serviceWorker.unregister();
