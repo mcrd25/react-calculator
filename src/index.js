149df3d564c7d9ca742aa2f653fc6f25b6f6c476
mcrd25 <mayacrd25@gmail.com>
2020-01-07 07:57:26 -0400

    Fixed linting errors, removed service worker

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import './index.css';
 
-import * as serviceWorker from './serviceWorker';
 
 function Title(props) {
-	return ( 
-		<h1>{props.value}</h1>
-	);
+  const { value } = props;
+  return (
+    <h1>{value}</h1>
+  );
 }
-
-ReactDOM.render( <Title value='My React Calculator' />, document.getElementById('root'));
-
-serviceWorker.unregister();
+Title.propTypes = {
+  value: PropTypes.string.isRequired,
+};
+ReactDOM.render(<Title value="My React Calculator" />, document.getElementById('root'));
