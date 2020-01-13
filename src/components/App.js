f214c410a9280d8a34fa6bf154d98ae4cc513328
mcrd25 <mayacrd25@gmail.com>
2020-01-13 03:11:33 -0400

    Basic App component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <div className="calculator">
+
+      </div>
+    );
+  }
+}
+
+export default App;
