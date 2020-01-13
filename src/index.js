e6e84a923ecfae17f480a7ed15ae86a1ba606f17
mcrd25 <mayacrd25@gmail.com>
2020-01-13 16:47:24 -0400

    Removed comments I forgot to remove

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-// import PropTypes from 'prop-types';
 import './index.css';
 
 
-// function Title(props) {
-//   const { value } = props;
-//   return (
-//     <h1>{value}</h1>
-//   );
-// }
-// Title.propTypes = {
-//   value: PropTypes.string.isRequired,
-// };
 ReactDOM.render(<App />, document.getElementById('root'));
