328f4fc46d0f60b86e325f70e63f2574bb911e7b
mcrd25 <mayacrd25@gmail.com>
2020-01-13 03:27:03 -0400

    Completed Basic display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,4 +1,26 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// class Display
+// eslint-disable-next-line react/prefer-stateless-function
+class Display extends React.Component {
+  render() {
+    const {
+      value,
+    } = this.props;
+    return (
+      <div className="calc-display">
+        {value}
+      </div>
+    );
+  }
+}
+
+Display.propTypes = {
+  value: PropTypes.string,
+};
+
+Display.defaultProps = {
+  value: 0,
+};
+
+export default Display;
